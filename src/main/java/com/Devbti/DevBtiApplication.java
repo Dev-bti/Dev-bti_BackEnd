6ccdd44138720d7c507416946426774b2e8884fd
rlawotmd <kimjs2400@naver.com>
2024-01-10 01:54:19 +0900

    init

---
+++ src/main/java/com/Devbti/DevBtiApplication.java
@@ -0,0 +1,13 @@
+package com.Devbti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Devbti/DevBtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Devbti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
