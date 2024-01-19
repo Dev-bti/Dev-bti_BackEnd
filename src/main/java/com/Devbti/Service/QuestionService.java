1e0b993d470ddd2340f4ea4f1ab34d5b855e636a
rlawotmd <kimjs2400@naver.com>
2024-01-20 02:04:24 +0900

    ✨ Question에 Answer 추가

--- src/main/java/com/Devbti/Service/QuestionService.java
+++ src/main/java/com/Devbti/Service/QuestionService.java
@@ -1,10 +1,13 @@
 package com.Devbti.Service;
 
+import com.Devbti.DTO.AnswerDTO;
 import com.Devbti.DTO.QuestionDTO;
+import com.Devbti.Repository.AnswerRepository;
 import com.Devbti.Repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,9 +17,26 @@ public class QuestionService {
     private QuestionRepository questionRepository;
 
     @Autowired
-    private AnswerService answerService;
 
+    private AnswerRepository answerRepository;
+
+    /*
     public List<QuestionDTO> getQuestions() {
-        return questionRepository.getQuestions();
+         //되는거
+        List<QuestionDTO> result = questionRepository.getQuestions();
+        return result;
+    }
+*/
+    public List<QuestionDTO> getQuestions() {
+        List<QuestionDTO> result = new ArrayList<>();
+        List<QuestionDTO> questions = questionRepository.getQuestions();
+
+        for (QuestionDTO question : questions) {
+            List<AnswerDTO> answers = answerRepository.getAnswersForQuestion(question.getQuestion_ID());
+            question.setAnswers(answers);
+            result.add(question);
+        }
+
+        return result;
     }
 }
