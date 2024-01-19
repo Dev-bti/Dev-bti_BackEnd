63977d1321af8c00860111589ea622908778653f
rlawotmd <kimjs2400@naver.com>
2024-01-20 00:59:59 +0900

    🩹 AnswerEntity 수정

--- src/main/java/com/Devbti/Entity/Answer.java
+++ src/main/java/com/Devbti/Entity/Answer.java
@@ -1,18 +1,27 @@
 package com.Devbti.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
 
 @Entity
 @Table(name = "answer_table")
+@IdClass(Answer.AnswerId.class)
 public class Answer {
     @Id
+    @Column(name = "Answer_ID")
     private Integer Answer_ID;
 /*    @ManyToOne
     @JoinColumn(name = "Question_ID")*/
+    @Id
+    @Column(name = "Question_ID")
     private Integer Question_ID;
     private String Answer_Sentence;
     private Integer Answer_Front;
     private Integer Answer_Back;
+
+    public static class AnswerId implements Serializable {
+        private Integer Answer_ID;
+        private Integer Question_ID;
+    }
 }
