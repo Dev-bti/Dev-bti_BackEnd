43ed8a635076498160074ae07c46696a60d901af
rlawotmd <kimjs2400@naver.com>
2024-01-23 00:05:22 +0900

    ⚡️ Override 추가

--- src/main/java/com/Devbti/Entity/Answer.java
+++ src/main/java/com/Devbti/Entity/Answer.java
@@ -11,8 +11,7 @@ public class Answer {
     @Id
     @Column(name = "Answer_ID")
     private Integer Answer_ID;
-/*    @ManyToOne
-    @JoinColumn(name = "Question_ID")*/
+
     @Id
     @Column(name = "Question_ID")
     private Integer Question_ID;
@@ -23,5 +22,24 @@ public class Answer {
     public static class AnswerId implements Serializable {
         private Integer Answer_ID;
         private Integer Question_ID;
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            AnswerId answerId = (AnswerId) obj;
+
+            if (Answer_ID != null ? !Answer_ID.equals(answerId.Answer_ID) : answerId.Answer_ID != null) return false;
+            return Question_ID != null ? Question_ID.equals(answerId.Question_ID) : answerId.Question_ID == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Answer_ID != null ? Answer_ID.hashCode() : 0;
+            result = 31 * result + (Question_ID != null ? Question_ID.hashCode() : 0);
+            return result;
+        }
     }
+
 }
