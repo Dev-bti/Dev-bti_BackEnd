4f873bbedc842b7a1925a2036ab9b6705e99e3df
rlawotmd <kimjs2400@naver.com>
2024-01-24 23:50:17 +0900

    ✨ Front, Back 점수 확인

--- src/main/java/com/Devbti/Controller/ResultController.java
+++ src/main/java/com/Devbti/Controller/ResultController.java
@@ -2,13 +2,10 @@ package com.Devbti.Controller;
 
 import com.Devbti.Service.ResultService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.*;
 
 @RestController
 @RequestMapping("/result")
@@ -17,8 +14,12 @@ public class ResultController {
     @Autowired
     private ResultService resultService;
 
-    @GetMapping
+    /*@GetMapping
     public ResponseEntity<List<>> getResult(@RequestParam int Front, int Back) {
 
+    }*/
+    @GetMapping
+    public int isit(@RequestParam int Front, int Back) {
+        return Front + Back;
     }
 }
