c16615937e08dfb226fa1bf15e428662148b5e69
rlawotmd <kimjs2400@naver.com>
2024-01-24 23:40:25 +0900

    ✨ ResultController 구현 중

--- src/main/java/com/Devbti/Controller/ResultController.java
+++ src/main/java/com/Devbti/Controller/ResultController.java
@@ -1,4 +1,19 @@
 package com.Devbti.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/result")
 public class ResultController {
+
+    @GetMapping
+    public ResponseEntity<List<>> getResult(@RequestParam int Front, int Back) {
+
+    }
 }
