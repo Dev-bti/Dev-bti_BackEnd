5fec4df60d252971fd1a8709111dd7c58f14a2c4
rlawotmd <kimjs2400@naver.com>
2024-01-27 23:47:16 +0900

    ✨ Result Controller 기능 추가

--- src/main/java/com/Devbti/Controller/ResultController.java
+++ src/main/java/com/Devbti/Controller/ResultController.java
@@ -1,11 +1,15 @@
 package com.Devbti.Controller;
 
+import com.Devbti.DTO.Result_TypeDTO;
 import com.Devbti.Service.ResultService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/result")
@@ -14,14 +18,18 @@ public class ResultController {
     @Autowired
     private ResultService resultService;
 
-    private int devType;
+    private Integer devType;
     @GetMapping
-    public ResponseEntity<List<>> getResult(@RequestParam int frontScore, int backScore) {
+    public ResponseEntity<List<Result_TypeDTO>> getResult(@RequestParam int frontScore, int backScore) {
+        if(frontScore >= backScore) {
+            if((frontScore - backScore) <= 10) devType = 0;
+            else devType = 1;
+        } else {
+            if((backScore - frontScore) <= 10) devType = 0;
+            else devType = 2;
+        }
 
+        List<Result_TypeDTO> result = resultService.getResult(devType);
+        return ResponseEntity.ok(result);
     }
-
-    /*@GetMapping
-    public int isit(@RequestParam int frontScore, int backScore) {
-        return frontScore + backScore;
-    }*/
 }
