c5b1c348c4f049c5203d45c86fde821fa1dc2bef
rlawotmd <kimjs2400@naver.com>
2024-01-27 23:48:15 +0900

    ➕ extends 추가

--- src/main/java/com/Devbti/Repository/Result_LanguageRepository.java
+++ src/main/java/com/Devbti/Repository/Result_LanguageRepository.java
@@ -1,7 +1,9 @@
 package com.Devbti.Repository;
 
+import com.Devbti.Entity.Result_Language;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Result_LanguageRepository {
+public interface Result_LanguageRepository extends JpaRepository<Result_Language, Integer> {
 }
--- src/main/java/com/Devbti/Repository/Result_WorkRepository.java
+++ src/main/java/com/Devbti/Repository/Result_WorkRepository.java
@@ -1,7 +1,9 @@
 package com.Devbti.Repository;
 
+import com.Devbti.Entity.Result_Work;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Result_WorkRepository {
+public interface Result_WorkRepository extends JpaRepository<Result_Work, Integer> {
 }
