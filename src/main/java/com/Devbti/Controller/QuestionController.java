f905e8dbd47c826a2a788bfc3e3921f500990729
rlawotmd <kimjs2400@naver.com>
2024-02-08 19:15:54 +0900

    ⚡️ cors 전체 허용 처리

--- src/main/java/com/Devbti/Controller/QuestionController.java
+++ src/main/java/com/Devbti/Controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.Devbti.DTO.QuestionDTO;
 import com.Devbti.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 //@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/question")
 public class QuestionController {
